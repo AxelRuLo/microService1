2c885206c72d3cd1c5bf859974faf3019328c44d
Axel Ruiz L <developeraxrulo@gmail.com>
2022-05-17 20:39:09 -0500

    servicio 2

--- app.js
+++ app.js
@@ -2,15 +2,12 @@ var express = require('express');
 var app = express();
 const port = process.env.PORT || 3000
 const pokemones = require("./routes/pokemons")
-const calculadora = require("./routes/operations")
-const files = require("./routes/files")
+
 
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use("/api/pokemon",pokemones)
-app.use("/api/calculadora",calculadora)
-app.use("/",files)
 
 app.listen(port, function () {
   console.log(`funcinando en el puerto ${port}`)
