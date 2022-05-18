ff489ed1fed02bbf6d6792dba236dc9fc6de20ec
Eduardo913 <cesarvilla903@gmail.com>
2022-05-18 00:50:15 -0500

    cors

--- app.js
+++ app.js
@@ -2,13 +2,15 @@ var express = require('express');
 var app = express();
 const port = process.env.PORT || 3000
 const pokemones = require("./routes/pokemons")
-const files = require("./routes/files")
+// const files = require("./routes/files")
 
+var cors = require("cors");
+app.use(cors()); 
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use("/api/pokemon",pokemones)
-app.use("/",files)
+// app.use("/",files)
 
 app.listen(port, function () {
   console.log(`funcinando en el puerto ${port}`)
